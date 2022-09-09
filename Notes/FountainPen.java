a40e7cabe87e972b95a7adef7a76d87f6abf6c56
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-09-09 11:37:48 -0700

    fountainPen Constructor assignment

---
+++ Notes/FountainPen.java
@@ -0,0 +1,24 @@
+import java.awt.Color;
+/**
+ * Write a description of class fountainPen here.
+ *
+ * @author Jose Prieto
+ * @version 09092022
+ */
+public class FountainPen
+{
+    private double nibSize;
+    private Color color;
+    private String material;
+    private String nibType;
+    private String inkStorage;
+
+    /**
+     * Constructor for fountainPen
+     */
+    public FountainPen(String material, String nibType, String inkStorage, Color color,
+    double nibSize){
+        
+    }
+}
+
