dcec9c1d3eea470e165658a747bf7a5898089e6f
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-09-12 11:38:41 -0700

    Worked on FountainPen, started Blackjack
    Work for 09/12

--- Notes/FountainPen.java
+++ Notes/FountainPen.java
@@ -1,6 +1,5 @@
-import java.awt.Color;
 /**
- * Write a description of class fountainPen here.
+ * Something something class constructor
  *
  * @author Jose Prieto
  * @version 09092022
@@ -8,17 +7,63 @@ import java.awt.Color;
 public class FountainPen
 {
     private double nibSize;
-    private Color color;
+    private String inkColor; //Color of ink
     private String material;
     private String nibType;
     private String inkStorage;
+    private double inkCapacity;
+    private double inkLevel;
 
     /**
      * Constructor for fountainPen
      */
-    public FountainPen(String material, String nibType, String inkStorage, Color color,
-    double nibSize){
-        
+    public FountainPen(String material, String nibType, String inkStorage, String inkColor,
+    double nibSize, double inkCapacity, double inkLevel){
+        this.material = material;
+        this.nibType = nibType;
+        this.inkStorage = inkStorage;
+        this.inkColor = inkColor;
+        this.nibSize = nibSize;
+        this.inkCapacity = inkCapacity;
+        this.inkLevel = inkLevel;
     }
+    
+    public FountainPen (){
+        this("Plastic", "Steel", "Eyedropper", "Black", 0.5, 1.0, 0.9); 
+        //Predetermined constructor 
+        //it's my Zebra fountain pen and is slightly used
+    }
+    
+    //Accessor Method
+    public double getInkLevel(){
+        return inkLevel;
+    }
+    
+    public double getInkCapacity(){
+        return inkCapacity;
+    }
+    
+    public double getNibSize(){
+        return nibSize;
+    }
+    
+    public String getInkColor(){
+        return inkColor;
+    }
+    
+    public String getInkStorage(){
+        return inkStorage;
+    }
+    
+    public String getNibType(){
+        return nibType;
+    }
+    
+    public String getMaterial(){
+        return material;
+    }
+    
+    //Mutator Method
+    
 }
 
