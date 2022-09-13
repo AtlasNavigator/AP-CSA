725d74afc1717785a7b2cf559f0d1cd8656939fb
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-09-13 11:38:32 -0700

    Object thing is done! 09/13

--- Notes/FountainPen.java
+++ Notes/FountainPen.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Something something class constructor
  *
@@ -27,43 +28,109 @@ public class FountainPen
         this.inkCapacity = inkCapacity;
         this.inkLevel = inkLevel;
     }
-    
+
     public FountainPen (){
         this("Plastic", "Steel", "Eyedropper", "Black", 0.5, 1.0, 0.9); 
         //Predetermined constructor 
         //it's my Zebra fountain pen and is slightly used
     }
-    
+
     //Accessor Method
     public double getInkLevel(){
         return inkLevel;
     }
-    
+
     public double getInkCapacity(){
         return inkCapacity;
     }
-    
+
     public double getNibSize(){
         return nibSize;
     }
-    
+
     public String getInkColor(){
         return inkColor;
     }
-    
+
     public String getInkStorage(){
         return inkStorage;
     }
-    
+
     public String getNibType(){
         return nibType;
     }
-    
+
     public String getMaterial(){
         return material;
     }
+
+    //Mutator Methods
+
+    public void setInkLevel(double newLevel){
+        inkLevel = newLevel;
+    }
+
+    public void setInkCapacity(double newCapacity){
+        inkCapacity = newCapacity;
+    }
+
+    public void setNibSize(double newNib){
+        nibSize = newNib;
+    }
+
+    public void setInkColor(String newInkColor){
+        inkColor = newInkColor;
+    }
+
+    public void setInkStorage(String newInkStorage){
+        inkStorage = newInkStorage;
+    }
+
+    public void setNibType(String newNibType){
+        nibType = newNibType;
+    }
+
+    public void setMaterial(String newMaterial){
+        material = newMaterial;
+    }
+
+    //Personal Methods for my Pen!
+
+    public void penUpgrade(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the nib shop, what nib would you like?");
+        nibType = scanner.next();
+        System.out.println();
+        System.out.println("Would you like to change our your ink reservoir?");
+        String answer = scanner.next();
+        if (answer.toLowerCase().equals("yes") ){ //Checks if it is equal
+            System.out.println("What would you like?");
+            inkStorage = scanner.next();
+        } else {
+            System.out.println("Thank you for stopping by, have a good day!");
+        }
+    }
+
+    public void write(){
+        if (inkLevel <= 0){
+            System.out.println("You're out of ink, please refill the pen.");
+        } else {
+            inkLevel -= 0.1;
+            System.out.println("Write amazing things!");           
+        }
+    }
     
-    //Mutator Method
-    
+    public void inkRefill(){
+        Scanner scanner = new Scanner(System.in);
+        inkLevel = 0;
+        System.out.println("You need to refill your pen. Would you like to do that?");
+        String answer = scanner.next();
+        if (answer.toLowerCase().equals("yes")){
+            setInkLevel(1);
+            System.out.println("Alright, your ink level is now at " + inkLevel + ".");
+        } else {
+            System.out.println("Just be aware your boss won't be too happy.");
+        }        
+    }
 }
 
