cba44d6f46e5d9ca09dcb93b88702d5d960bdc2d
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-09-16 11:17:21 -0700

    Getting close to done with Blackjack 09/16

--- Notes/blackjackGame.java
+++ Notes/blackjackGame.java
@@ -7,47 +7,63 @@ import java.util.Scanner;
  */
 public class blackjackGame
 {   
-    Scanner scanner = new Scanner(System.in);
-
-    private boolean winCheck(int total){
+    private static boolean winCheck(int total){
         if (total == 21){
+            System.out.println("");
             System.out.println("Congratulations, you won! You got 21.");
             return true;
         } else {
             return false;
         }
     }
-    
+
     public static void game(){ //This is the game's method
+        Scanner scanner = new Scanner(System.in);
+
         /**
          * Helps differentiate between the player and dealer's hands.
          */
         hand player = new hand();  //Player's hand
         hand dealer  = new hand(); //Dealer's hand
-        
+
         System.out.println("#################  WELCOME TO BLACKJACK!  #################");
         //Game welcome message
-        
+
         /**
          * Player initial hand
          */
-        
+
         System.out.println("");
         System.out.println("-------- PLAYER --------"); //Player hand
         System.out.println("Card 1: " + player.card1Value()); //Print card 1 for player
         System.out.println("Card 2: " + player.card2Value()); //Print card 2 for player
         System.out.println("Total: " + player.getTotal()); //Print player total
-        
+
         /**
          * Dealer's hand from POV of player
          */
-        
+
         System.out.println("");
         System.out.println("-------- DEALER --------"); //Dealer's initial card
         System.out.println("Card 1: " + dealer.card1Value()); //Print dealer's 1st card
         System.out.println("Card 2: Undefined"); //Do not reveal card 2
-        
+
         //Begin the actual game
-        
+
+        boolean playGame = true; //Is the game playing
+        while (playGame){
+            System.out.println("What would you like to do?");
+            System.out.println("1. Hit");
+            System.out.println("2. Stand");
+            int response = scanner.nextInt();
+
+            if (response == 1){
+                player.hit(); //Executes the hit command
+                
+                if (winCheck(player.getTotal())){
+                    
+                }
+            }
+        }
     }
 }
