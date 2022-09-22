2b42c3c10a8b333cc7cc872ba5034e5f675c700d
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-09-22 11:36:25 -0700

    Working on magpie Chatbot- 09/22
    Just need to add a few keywords, almost complete.

---
+++ activity2/questionsAnswers.java
@@ -0,0 +1,48 @@
+
+/**
+ * Answers to questions from the Magpie Lab Student Guide
+ *
+ * @author Jose Prieto
+ * @version 09222022
+ */
+public class questionsAnswers
+{
+    /*
+     * Exploration:
+     * 
+     * 1. Magpie responds to questions such as "Where do you come from?" with
+     * "Do you really think so?" or "Tell me more." She gives no real response.
+     * With it.
+     * 
+     * 2. She will randomly say "Tell me more," even if the response typed is blank.
+     * 
+     * 3. She will give a generic response.
+     * 
+     * Questions:
+     * 
+     * 1. Yes, it looks for words such as 'family' and 'no.'
+     * 
+     * 2. Family might trigger the bot to ask for more about your family.
+     * 
+     * Magpie Response Prompts:
+     * 
+     * "My mother and I talked last night."
+     * Magpie will respond my requesting for me to tell her more about my family.
+     * 
+     * "I said no!"
+     * Magpie asks me why I am so negative.
+     * 
+     * "The weather is nice."
+     * Magpie will either respond with 'Hmm' or 'Tell me more.'
+     * 
+     * "Do you know my brother?"
+     * Magpie responds with "Tell me more about your family."
+     * 
+     * Code Modifications:
+     * 
+     * Pets - Complete
+     * Teacher - Complete
+     * nullResponse - Done!
+     * 2 extra nocommital responses - Check
+     */
+}
