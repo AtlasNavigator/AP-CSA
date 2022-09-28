070af21345045795b0b8745c473e2de7ee7477fb
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-09-28 10:53:47 -0700

    Add Practice FRQ folder 09/28

---
+++ Tax Calculator Side Project/Tax Calculator Side Project/taxCalculator.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class taxCalculator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class taxCalculator
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class taxCalculator
+     */
+    public taxCalculator()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
