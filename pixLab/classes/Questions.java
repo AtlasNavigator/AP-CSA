56e1a5e7f35cc19ac4372b1c112a9d75b4e8250f
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2022-12-05 11:06:32 -0800

    A3 Done PixLab 12/5

---
+++ pixLab/classes/Questions.java
@@ -0,0 +1,27 @@
+
+/**
+ * Question answers for the Picture Lab Student Guide
+ *
+ * @author Jose Prieto
+ * @version 12/02/2022
+ */
+public class Questions
+{
+    /*
+     * 1. 255, 83, 247
+     * 2. 255, 255, 0
+     * 3. 255, 0, 255
+     * 4. 255, 255, 0
+     * 5. 90, 90, 90
+     * 
+     * A3:
+     * 
+     * 1. 0
+     * 2. 0
+     * 3. 0
+     * 4. 480
+     * 5. top to bottom
+     * 6. left to right
+     * 7. Yes, pixels can be seen
+     */
+}
