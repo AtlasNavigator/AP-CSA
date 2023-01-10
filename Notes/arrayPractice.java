3e57d01d1e70be472e80388845170bf612a0bd08
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2023-01-10 11:25:46 -0800

    removeIndex Complete 01/10

--- Notes/arrayPractice.java
+++ Notes/arrayPractice.java
@@ -12,13 +12,13 @@ public class arrayPractice
      */
     int[] arr;
 
-    public void arrayPractice(){
+    public arrayPractice(){
         arr = new int[6]; //Create array with 6 slots
         for (int i = 0; i < arr.length; i++){
             arr[i] = (int)(Math.random()*10 + 1); //Fill the array with random nums
         }
     }
-    
+
     /**
      * Prints current contents of array in a single line with a space
      * between each number
@@ -27,8 +27,9 @@ public class arrayPractice
         for (int num : arr){
             System.out.print(num + " ");
         }
+        System.out.println();
     }
-    
+
     /**
      * Removes the ith number form the list, and replaces last index with 0
      * 
@@ -36,12 +37,12 @@ public class arrayPractice
      * with the call removeIndex(2) becomes: 2 7 1 4 10 0
      */
     public void removeIndex(int index){
-        int[] arr2;
-        arr2 = new int[6];
-        
-        //Not done, finish tomorrow (01/09)
+        for (int i = index; i < arr.length - 1; i++){
+            arr[i] = arr[i+1];
+        }
+        arr[arr.length - 1] = 0;
     }
-    
+
     //Part B
     /**
      * Adds num to the array at specified index. To accomodate the change,
@@ -50,7 +51,7 @@ public class arrayPractice
      * For example: 2 7 3 1 4 10
      * with the call AddNum(9, 1) becomes: 2 9 7 3 1 4
      */
-    
+
     public void addNum(){
         
     }
