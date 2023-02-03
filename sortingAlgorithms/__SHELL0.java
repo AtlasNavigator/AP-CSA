9ed62c5d6c07d8e239d7fbe4153f158fdc699128
Jose Prieto <92605951+AtlasNavigator@users.noreply.github.com>
2023-02-03 11:41:54 -0800

    mostly working bubbleSort

---
+++ sortingAlgorithms/__SHELL0.java
@@ -0,0 +1,10 @@
+
+public class __SHELL0 extends bluej.runtime.Shell {
+public static java.lang.Object run() throws Throwable {
+
+try {
+return makeObj(bubbleSort.bubblesort()
+);}
+finally {
+}
+}}
--- sortingAlgorithms/bubbleSort.java
+++ sortingAlgorithms/bubbleSort.java
@@ -8,5 +8,28 @@ import java.util.*;
  */
 public class bubbleSort
 {
-   
+    public static int[] bubblesort() {
+        int[] numbers;
+        numbers = new int[1000000];
+        for (int i = 0; i < numbers.length - 1; i++){
+            int randomNum = (int)(Math.random() * 32000);
+            numbers[i] = randomNum;
+        }
+        
+        boolean swap = true;
+        System.out.println("Beginning bubbleSort");
+        for(int i = numbers.length - 1; i > 0 && swap; i--) {
+            swap = false;
+            for (int j = 0; j < i; j++) {
+                if (numbers[j] > numbers[j+1]) {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j+1];
+                    numbers[j+1] = temp;
+                    swap = true;
+                    System.out.println("Swap at index " + j);
+                }
+            }
+        }
+        return numbers;
+    }
 }
